1399e89b7b18fe84bf4a850104f8c2a0ce2c7b1b
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 01:51:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :matriarch => {}
-      :hero => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {}
-      :matriarch => {}
-      :heroine => {}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
       }
-    
   }
 
   
